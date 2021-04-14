cb3ea97cb9d62c108382ac5bd9b23baeb42048c9
Rodrigo Teixeira <rodrigoteixeira52@hotmail.com>
2021-04-14 16:20:52 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,41 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Luiz Inacio Lula da Silva',
+                partido: 'PT',
+                vice: 'Boulos',
+                fotos: [
+                    {url:'lula.jpg', legenda: 'Presidente'},
+                    {url:'boulos.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+
+            },
+            {
+                numero: '09',
+                nome: 'Adriano Imperador',
+                partido: 'CV',
+                vice: 'Ronaldinho Gaucho',
+                fotos: [
+                    {url:'didico.jpg', legenda: 'Presidente'},
+                    {url:'bruxo.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+
+            },
+            {
+                numero: '45',
+                nome: 'Ciro Gomes',
+                partido: 'PDT',
+                vice: 'Marina',
+                fotos: [
+                    {url:'ciro.jpg', legenda: 'Presidente'},
+                    {url:'marina.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+
+            }
+        ]
+    }
+]
